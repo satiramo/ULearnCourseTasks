c37e605aed47b5763d2c8cc1dc651e7eaa8c7eb2
satiramo <satiramo@gmail.com>
2020-04-11 05:46:50 +0500

    Modify TextAnalysis

--- TextAnalysis.csproj/TextGeneratorTask.cs
+++ TextAnalysis.csproj/TextGeneratorTask.cs
@@ -9,6 +9,21 @@ namespace TextAnalysis
             string phraseBeginning,
             int wordsCount)
         {
+            if (nextWords.Count > 0 && wordsCount > 0)
+            {
+                for (var i = 0; i < wordsCount; i++)
+                {
+                    var wordsInPhraseBeginingArray = phraseBeginning.Split(' ');
+                    var arrayLength = wordsInPhraseBeginingArray.Length;                    
+                    var lastWord = wordsInPhraseBeginingArray[arrayLength - 1];
+                    if (arrayLength > 1 && nextWords.ContainsKey(string.Format("{0} {1}", wordsInPhraseBeginingArray[arrayLength - 2], lastWord)))
+                        phraseBeginning += " " + nextWords[string.Format("{0} {1}", wordsInPhraseBeginingArray[arrayLength - 2], lastWord)];
+                    else if (nextWords.ContainsKey(lastWord))
+                        phraseBeginning += " " + nextWords[lastWord];
+                    else
+                        return phraseBeginning;
+                }
+            }
             return phraseBeginning;
         }
     }
